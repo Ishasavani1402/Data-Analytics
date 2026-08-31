29510757050958ec07cfc0136599dee98f8cd18e
Ishasavani1402 <savaniisha57@gmail.com>
2026-08-31 17:18:17 +0530

    save changes

---
+++ walmart_sql/sql analysis/data_clean.sql
@@ -0,0 +1,119 @@
+ select * from sales;
+ 
+-- 1 . total record--
+select count(*) as total_record from sales;
+
+-- 2 . check invoice id (duplicate or not) --
+select invoice_id , count(*) as total from sales group by invoice_id
+having count(*) > 1; 
+
+-- 3. check null value --
+SELECT
+SUM(invoice_id IS NULL) invoice_id,
+SUM(branch IS NULL) branch,
+SUM(city IS NULL) city,
+SUM(customer_type IS NULL) customer_type,
+SUM(gender IS NULL) gender,
+sum(product_name is null) product_name , 
+SUM(product_line IS NULL) product_line,
+SUM(unit_price IS NULL) unit_price,
+SUM(quantity IS NULL) quantity,
+SUM(vat IS NULL) vat,
+SUM(total IS NULL) total,
+SUM(date IS NULL) sale_date,
+SUM(time IS NULL) sale_time,
+SUM(payment IS NULL) payment,
+SUM(cogs IS NULL) cogs,
+SUM(gross_margin_pct IS NULL) gross_margin_pct,
+SUM(gross_income IS NULL) gross_income,
+SUM(rating IS NULL) rating , 
+sum(product_category is null) product_category
+FROM sales;
+-- ans : no null value found
+
+-- 4. Check Blank Values --
+SELECT *
+FROM sales
+WHERE TRIM(city)=''
+OR TRIM(branch)=''
+OR TRIM(customer_type)=''
+OR TRIM(product_line)=''
+or trim(product_name)=''
+or trim(product_category)=''
+OR TRIM(payment)='';
+-- ans : no blank found
+
+-- 5 . remove extra spaces
+UPDATE sales
+SET
+city = TRIM(city),
+branch = TRIM(branch),
+customer_type = TRIM(customer_type),
+gender = TRIM(gender),
+product_line = TRIM(product_line),
+product_name = trim(product_name) , 
+product_category = trim(product_category) , 
+payment = TRIM(payment);
+
+-- 6 . verify data types
+describe sales;
+
+-- 7 . velidate if 0 or negetive value occure --
+select * from sales where quantity <= 0 or
+total <= 0 or unit_price <= 0 or gross_margin_pct <= 0 
+or rating <= 0;
+
+-- 8 . check unique values
+SELECT
+    'branch' AS column_name,
+    COUNT(DISTINCT branch) AS unique_count,
+    GROUP_CONCAT(DISTINCT branch ORDER BY branch SEPARATOR ', ') AS unique_values
+FROM sales
+
+UNION ALL
+
+SELECT
+    'city',
+    COUNT(DISTINCT city),
+    GROUP_CONCAT(DISTINCT city ORDER BY city SEPARATOR ', ')
+FROM sales
+
+UNION ALL
+
+SELECT
+    'customer_type',
+    COUNT(DISTINCT customer_type),
+    GROUP_CONCAT(DISTINCT customer_type ORDER BY customer_type SEPARATOR ', ')
+FROM sales
+
+UNION ALL
+
+SELECT
+    'gender',
+    COUNT(DISTINCT gender),
+    GROUP_CONCAT(DISTINCT gender ORDER BY gender SEPARATOR ', ')
+FROM sales
+
+UNION ALL
+
+SELECT
+    'product_line',
+    COUNT(DISTINCT product_line),
+    GROUP_CONCAT(DISTINCT product_line ORDER BY product_line SEPARATOR ', ')
+FROM sales
+
+UNION ALL
+
+SELECT
+    'payment',
+    COUNT(DISTINCT payment),
+    GROUP_CONCAT(DISTINCT payment ORDER BY payment SEPARATOR ', ')
+FROM sales
+
+UNION ALL
+
+SELECT
+    'product_category',
+    COUNT(DISTINCT product_category),
+    GROUP_CONCAT(DISTINCT product_category ORDER BY product_category SEPARATOR ', ')
+FROM sales;
---
+++ walmart_sql/sql analysis/prepare_dataset.sql
@@ -0,0 +1,38 @@
+create database if not exists walmart;
+use walmart;
+
+-- table creation----
+create table sales(
+invoice_id varchar(50) not null primary key , 
+branch varchar(30) not null , 
+city varchar(30) not null , 
+customer_type varchar (50) not null , 
+gender varchar(20) not null , 
+product_name varchar (50) not null , 
+product_line varchar (50) not null , 
+unit_price decimal (10,2) not null , 
+quantity int not null , 
+vat decimal (6,4) not null , 
+total decimal (12,4) not null , 
+date datetime not null , 
+time time not null , 
+payment varchar (50) not null , 
+cogs decimal (10 ,2) not null ,
+gross_margin_pct decimal (12,4) , 
+gross_income decimal (12 , 4) , 
+rating decimal (2,1) ,
+ product_category varchar (50));
+ 
+ --- load data---
+ 
+ show global variables like 'local_infile'; -- we can load data from local machine 
+ set global local_infile = 1;
+ 
+ load data local infile
+ 'D:/Data Analytics/walmart_sql/dataset/Walmart_Sales.csv'
+ into table sales
+ fields terminated by ','
+ enclosed by '"'
+ lines terminated by '\n'
+ ignore 1 rows;
+ 
